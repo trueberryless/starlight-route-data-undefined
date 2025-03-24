824f081702685bfcf564587c1ff46b199bc5e46b
trueberryless <trueberryless+github@gmail.com>
2025-03-24 15:48:52 +0100

    additional example: not even starlightRoute is defined in the same scenario

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,14 +13,7 @@ export default defineConfig({
       sidebar: [
         {
           label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          autogenerate: { directory: "guides" },
         },
       ],
       plugins: [
